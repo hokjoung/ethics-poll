2128e9b458d4526f82ac929060be67795daa1dc9
hokjoung <hokjoung@tosspayments.com>
2021-12-07 23:37:47 +0900

    bug fix - annotations for spring entity manager

--- storage/db-core/src/main/kotlin/kr/ac/kaist/ethics/db/core/school/Answer.kt
+++ storage/db-core/src/main/kotlin/kr/ac/kaist/ethics/db/core/school/Answer.kt
@@ -1,8 +1,26 @@
 package kr.ac.kaist.ethics.db.core.school
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "answer")
 data class Answer(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     val id: Long = 0L,
+
+    @Column(name = "ref_question_id")
     val refQuestionId: Long,
+
+    @Column(name = "client_id")
     val clientId: String?,
+
+    @Column(name = "selection")
     val selection: Int
 )
--- storage/db-core/src/main/kotlin/kr/ac/kaist/ethics/db/core/school/Question.kt
+++ storage/db-core/src/main/kotlin/kr/ac/kaist/ethics/db/core/school/Question.kt
@@ -1,8 +1,26 @@
 package kr.ac.kaist.ethics.db.core.school
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "question")
 data class Question(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     val id: Long = 0L,
+
+    @Column(name = "question")
     val question: String,
+
+    @Column(name = "option1")
     val option1: String,
+
+    @Column(name = "option2")
     val option2: String
 )
