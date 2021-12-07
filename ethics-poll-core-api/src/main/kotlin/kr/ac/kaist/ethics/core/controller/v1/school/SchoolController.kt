2ed7f4321020e89388ac205b692bfcf44c36a237
hokjoung <hokjoung@tosspayments.com>
2021-12-07 23:07:12 +0900

    implement list api

--- ethics-poll-core-api/src/main/kotlin/kr/ac/kaist/ethics/core/controller/v1/school/SchoolController.kt
+++ ethics-poll-core-api/src/main/kotlin/kr/ac/kaist/ethics/core/controller/v1/school/SchoolController.kt
@@ -3,8 +3,13 @@ package kr.ac.kaist.ethics.core.controller.v1.school
 import kr.ac.kaist.ethics.core.controller.v1.school.reqres.QuestionDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @RestController
@@ -13,16 +18,14 @@ class SchoolController(
     val service: SchoolService
 ) {
     @GetMapping(value = ["list"])
-    fun list(): List<QuestionDto> {
-        /*
-        service.getQuestionList()
-        servletResponse.addCookie()
-         */
-        return emptyList()
+    fun list(servletResponse: HttpServletResponse): List<QuestionDto> {
+        val res = service.getList()
+        servletResponse.addCookie(Cookie("sessionid", UUID.randomUUID().toString()))
+        return res
     }
 
     @PostMapping(value = ["answer"])
-    fun answer() {
+    fun answer(/* servletRequest: HttpServletRequest, @RequestBody req: AnswerRequest */) {
         /*
         service.newAnswer(user, )
          */
--- ethics-poll-core-api/src/main/kotlin/kr/ac/kaist/ethics/core/controller/v1/school/SchoolService.kt
+++ ethics-poll-core-api/src/main/kotlin/kr/ac/kaist/ethics/core/controller/v1/school/SchoolService.kt
@@ -1,5 +1,7 @@
 package kr.ac.kaist.ethics.core.controller.v1.school
 
+import kr.ac.kaist.ethics.core.controller.v1.school.reqres.AnswerStatDto
+import kr.ac.kaist.ethics.core.controller.v1.school.reqres.QuestionDto
 import kr.ac.kaist.ethics.db.core.school.AnswerRepository
 import kr.ac.kaist.ethics.db.core.school.QuestionRepository
 import org.springframework.stereotype.Service
@@ -9,5 +11,22 @@ class SchoolService(
     val questionRepo: QuestionRepository,
     val answerRepo: AnswerRepository
 ) {
+    fun getList(): List<QuestionDto> {
+        val questionList = questionRepo.findAll()
+        val answerList = answerRepo.findAll()
+        questionList.map { question ->
+            val answersForQuestion = answerList.filter { it.refQuestionId == question.id }
+            QuestionDto(
+                question = question.question,
+                option1 = question.option1,
+                option2 = question.option2,
+                answerStats = AnswerStatDto(
+                    option1Count = answersForQuestion.count { it.selection == 1 },
+                    option2Count = answersForQuestion.count { it.selection == 2 }
+                )
+            )
+        }
+    }
+
 
 }
